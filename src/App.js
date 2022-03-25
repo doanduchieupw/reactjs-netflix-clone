d246538cb95fb3db8107f20cb4bd29ad130239b6
doanduchieupw <doanduchieu.pw12@gmail.com>
2022-03-25 14:42:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FooterContainer } from './containers/footer';
+import { JumbotronContainer } from './containers/jumbotron';
+
+
+function App() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Jumbotron } from './jumbotron';
+export { default as Footer } from './footer';
+
+
+
